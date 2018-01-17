8a4270468099dfc1f35e3a1e65ac7ae2af6b59df
Marc Villagrasa <marcvege@gmail.com>
2018-01-18 00:09:33 +0100

    Small refactor

--- src/main/java/Formula.java
+++ src/main/java/Formula.java
@@ -1,8 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Formula {
     private String formula;
 
     public Formula(String formula) {
         this.formula = formula;
+    }
+
+    public Map<String,Integer> getAtoms(HashMap<String, Integer> counter) {
+        if(this.isEmpty()) return counter;
+//        return formula.rest().getAtoms(counter.add(formula.first().getAtoms()))
+        return null;
     }
 
     public boolean isEmpty() {
--- src/main/java/ParseMolecule.java
+++ src/main/java/ParseMolecule.java
@@ -4,12 +4,7 @@ import java.util.Map;
 public class ParseMolecule {
 
     public static Map<String,Integer> getAtoms(String formula) {
-        return getAtoms(new Formula(formula), new HashMap<String, Integer> ());
-    }
-
-    private static Map<String,Integer> getAtoms(Formula formula, HashMap<String, Integer> counter) {
-        if(formula.isEmpty()) return counter;
-        return null;
+        return new Formula(formula).getAtoms(new HashMap<String, Integer> ());
     }
 
 }
