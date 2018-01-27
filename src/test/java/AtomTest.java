64f25c7c58949b4a01bf966333513645f77e5150
Marc Villagrasa <marc.villagrasa@mango.com>
2018-01-27 12:18:22 +0100

    Change gitignore

--- src/test/java/AtomTest.java
+++ src/test/java/AtomTest.java
@@ -1,26 +1,23 @@
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AtomShould {
     @Test
     void create_an_empty_element() {
         Atom atom = (Atom) Atom.extract("O").get();
-        assertThat(atom.element, is("O"));
-        assertThat(atom.number, is(1));
+        assertThat(atom).isEqualToComparingFieldByField(new Atom("O","O", 1));
     }
 
     @Test
-    void create_an_numered_element() {
+    void create_an_numeral_element() {
         Atom atom = (Atom) Atom.extract("Li2").get();
-        assertThat(atom.element, is("Li"));
-        assertThat(atom.number, is(2));
+        assertThat(atom).isEqualToComparingFieldByField(new Atom("Li2","Li", 2));
     }
 
     @Test
     void extract_atom_when_formula_start_with_atom() {
         Atom atom = (Atom) Atom.extract("H2O").get();
-        assertThat(atom, is(new Atom("H2", "H", 2)));
+        assertThat(atom).isEqualToComparingFieldByField(new Atom("H2", "H", 2));
     }
 }
