8894d7cbfa969e4d917e14dba3f023f01f081303
Marc Villagrasa <marc.villagrasa@mango.com>
2018-01-27 12:20:32 +0100

    Rename test to Should

--- src/test/java/AtomShould.java
+++ src/test/java/AtomShould.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AtomTest {
+class AtomShould {
     @Test
     void create_an_empty_element() {
         Atom atom = (Atom) Atom.extract("O").get();
